30c74f839779687c3d6d1759d60170fae4108d5e
Hassan Saber <hassansaberkarin@gmail.com>
2022-03-25 19:15:25 +0430

    ز

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-aa;
+const getData = async () => {
+  try {
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+    const data = await response.json();
+    episodeData(data);
+  } catch (error) {
+    throw new Error("not found...", error);
+  }
+};
+getData();
+
+const episodeData = (data) => {
+  data.forEach((episode) => {
+    console.log(episode);
+  });
+};
